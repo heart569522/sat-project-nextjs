e56aaae29f8f5eeec0e7c825b949c6e4f6b2cf97
Heart Wirunrom <heart569522@gmail.com>
2023-12-09 01:50:08 +0700

    loading screen

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,12 @@
+import { CircularProgress } from "@mui/material";
+
+export default function loading() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <CircularProgress />
+      <span className="ml-4 text-lg font-medium">
+        Loading...
+      </span>
+    </div>
+  );
+}
--- app/dashboard/test/page.tsx
+++ app/dashboard/test/page.tsx
@@ -1,19 +1,19 @@
 import PN01Form from '@/app/components/form/pn01-form';
 import { Metadata } from 'next';
 
-
 export const metadata: Metadata = {
-    title: 'Test Page',
+  title: 'แบบฟอร์มเสนอโครงการ/กิจกรรม (พน.01)',
 };
 
-export default async function Page({ }: {}) {
-    return (
-        <div className="w-full">
-            <div className="flex w-full items-center justify-between">
-                <h1 className={`text-2xl`}>Test</h1>
-            </div>
-            <PN01Form />
-
-        </div>
-    );
+export default async function Page({}: {}) {
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`text-2xl`}>แบบฟอร์มเสนอโครงการ/กิจกรรม (พน.01)</h1>
+      </div>
+      <div className='mt-4'>
+        <PN01Form />
+      </div>
+    </div>
+  );
 }
