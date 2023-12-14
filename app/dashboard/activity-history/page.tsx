e0872110ae105ec63e92be9011bb7cf14bc98684
Heart Wirunrom <heart569522@gmail.com>
2023-12-14 22:51:19 +0700

    fix head title

--- app/dashboard/activity-history/page.tsx
+++ app/dashboard/activity-history/page.tsx
@@ -1,5 +1,6 @@
-import PN11Form from '@/app/components/form/pn11-form';
 import { Metadata } from 'next';
+import Link from 'next/link';
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
 
 export const metadata: Metadata = {
   title: 'ประวัติการเข้าร่วมโครงการ/กิจกรรม',
@@ -11,8 +12,16 @@ export default async function Page({}: {}) {
       <div className="flex w-full items-center justify-between">
         <h1 className={`text-2xl`}>ประวัติการเข้าร่วมโครงการ/กิจกรรม</h1>
       </div>
-      <div className='mt-4'>
-        <PN11Form />
+      <div className="mt-4">
+        <div className="mt-6 flex justify-start gap-4">
+          <Link
+            href="/dashboard/activity-history/pn11"
+            className="flex h-12 md:h-20 items-center rounded-lg bg-blue-600 px-4 text-lg font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          >
+            <span className="hidden md:block">ร้องขอระเบียนกิจกรรม <br />Activity Transcript</span>{' '}
+            <DescriptionOutlinedIcon className="h-8 w-8 md:ml-2" />
+          </Link>
+        </div>
       </div>
     </div>
   );
--- app/dashboard/activity-history/pn11/page.tsx
+++ app/dashboard/activity-history/pn11/page.tsx
@@ -1,4 +1,5 @@
 import PN11Form from '@/app/components/form/pn11-form';
+import Breadcrumbs from '@/app/components/invoices/breadcrumbs';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -7,13 +8,22 @@ export const metadata: Metadata = {
 
 export default async function Page({}: {}) {
   return (
-    <div className="w-full">
-      <div className="flex w-full items-center justify-between">
-        <h1 className={`text-2xl`}>คำร้องขอหลักฐานการเข้าร่วมโครงการ (พน.11)</h1>
+    <>
+      <div>
+        <Breadcrumbs
+          breadcrumbs={[
+            { label: 'ประวัติการเข้าร่วมโครงการ/กิจกรรม', href: '/dashboard/activity-history' },
+            {
+              label: 'แบบร้องขอหลักฐานการเข้าร่วมโครงการ (พน.11)',
+              href: '/dashboard/activity-history/pn11',
+              active: true,
+            },
+          ]}
+        />
       </div>
-      <div className='mt-4'>
+      <div className="w-full mt-4">
         <PN11Form />
       </div>
-    </div>
+    </>
   );
 }
