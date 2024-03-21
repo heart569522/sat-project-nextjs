c05afa6893e25266c3e63feb96ccbdc91d1e3716
Heart Wirunrom <heart569522@gmail.com>
2024-03-22 03:38:19 +0700

    fix ap

--- app/api/majors/route.ts
+++ app/api/majors/route.ts
@@ -1,7 +1,9 @@
 import { pool } from '@/app/lib/db';
 import { NextRequest, NextResponse } from 'next/server';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function GET() {
+  noStore();
   try {
     const res = await pool.query(
       `SELECT * FROM majors WHERE is_delete = false ORDER BY id`,
