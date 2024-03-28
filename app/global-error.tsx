48c5df69b11e6675bb303b6f951c148c2f5664b3
Heart Wirunrom <heart569522@gmail.com>
2024-03-29 05:04:58 +0700

    del unuse

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { FaceFrownIcon } from '@heroicons/react/24/outline';
+
 export default function GlobalError({
   error,
   reset,
@@ -10,8 +12,16 @@ export default function GlobalError({
   return (
     <html>
       <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <main className="flex h-full flex-col items-center justify-center gap-2">
+          <FaceFrownIcon className="w-10 text-gray-400" />
+          <h2 className="text-xl font-semibold">Something went wrong!</h2>
+          <button
+            onClick={() => reset()}
+            className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+          >
+            Try again
+          </button>
+        </main>
       </body>
     </html>
   );
